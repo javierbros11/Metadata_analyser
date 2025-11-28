71e50fe1a3ff910081bbb3dc868f0ac984016e0d
Javier Limón Polo <javierlimonpolo@gmail.com>
2025-11-28 12:23:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from metadata_analyser import extract_metadata
 # Inicio del programa
 
 if __name__ == "__main__":
-    filepath = "ruta_acceso"
+    filepath = input("Dime la ruta del archivo a analizar: ")
     metadata = extract_metadata(filepath)
     
     for key, value in metadata.items():
